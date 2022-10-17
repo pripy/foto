ebf59c321b73f0622ac9b85c002990654a28cbd6
pripy <110524316+pripy@users.noreply.github.com>
2022-10-17 10:09:46 +0300

    Add files via upload

---
+++ pripyaw2/detsad_radost.js
@@ -0,0 +1,5 @@
+document.write('<i>https://ic.wampi.ru/2022/10/08/PRI5-DETSAD-RADOST-01.jpg</i><b>при5 детсад радость 01.jpg</b>');
+document.write('<i>https://ie.wampi.ru/2022/10/08/PRI5-DETSAD-RADOST-02.jpg</i><b>при5 детсад радость 02.jpg</b>');
+document.write('<i>https://im.wampi.ru/2022/10/08/PRI5-DETSAD-RADOST-03.jpg</i><b>при5 детсад радость 03.jpg</b>');
+document.write('<i>https://ic.wampi.ru/2022/10/08/PRI5-DETSAD-RADOST-04.jpg</i><b>при5 детсад радость 04.jpg</b>');
+document.write('<i>https://ie.wampi.ru/2022/10/08/PRI5-DETSAD-RADOST-05.jpg</i><b>при5 детсад радость 05.jpg</b>');
