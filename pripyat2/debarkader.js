ae2b1cb1f4ab074638ddeb60df805b8fa090e2e9
pripy <110524316+pripy@users.noreply.github.com>
2022-10-17 09:39:58 +0300

    Add files via upload

---
+++ pripyat2/debarkader.js
@@ -0,0 +1,48 @@
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-019824242ace86dffb.jpg</i><b>при5 дебаркадер 01</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-02aae948edfed3d4ea.jpg</i><b>при5 дебаркадер 02</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-0367ac6213c9557777.jpg</i><b>при5 дебаркадер 03</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-047dd190b2d009c13e.jpg</i><b>при5 дебаркадер 04</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-05dd7a49789986d810.jpg</i><b>при5 дебаркадер 05</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-0670c479d14797c889.jpg</i><b>при5 дебаркадер 06</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-07f7409c07646e8189.jpg</i><b>при5 дебаркадер 07</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-0877b4244318daf303.jpg</i><b>при5 дебаркадер 08</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-099ff729682e293bc4.jpg</i><b>при5 дебаркадер 09</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-102353028b3c5ccb9e.jpg</i><b>при5 дебаркадер 10</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-01be99a86af108e357.jpg</i><b>при5 дебаркадер верх 01</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-027c2be9195e9022ca.jpg</i><b>при5 дебаркадер верх 02</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-0315be40ffbdabb4a8.jpg</i><b>при5 дебаркадер верх 03</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-04f80834a61e6b0b03.jpg</i><b>при5 дебаркадер верх 04</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-051fc6581bdacd74eb.jpg</i><b>при5 дебаркадер верх 05</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-060097df914a3924e6.jpg</i><b>при5 дебаркадер верх 06</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-078adec29c6a6c1ec2.jpg</i><b>при5 дебаркадер верх 07</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-08dc627ec0bab80b2c.jpg</i><b>при5 дебаркадер верх 08</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-VERK-095d58a1003674f003.jpg</i><b>при5 дебаркадер верх 09</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-01fc80f487418ccfa7.jpg</i><b>при5 дебаркадер до 01</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-02db3e56e5329631ea.jpg</i><b>при5 дебаркадер до 02</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-0368af77b58bae7c3e.jpg</i><b>при5 дебаркадер до 03</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-04a43b53846433cc8d.jpg</i><b>при5 дебаркадер до 04</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-05f961866a5a5ed194.jpg</i><b>при5 дебаркадер до 05</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-0618b82e3b3a7ff1e2.jpg</i><b>при5 дебаркадер до 06</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-07609bc810d1f67b1e.jpg</i><b>при5 дебаркадер до 07</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-DO-08bcc2ed973e68fbb6.jpg</i><b>при5 дебаркадер до 08</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-0197691bc1d2ea54dc.jpg</i><b>при5 дебаркадер каюта 01</b>');
+
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-0348dfde2d0778193b.jpg</i><b>при5 дебаркадер каюта 03</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-04454aba34852351ba.jpg</i><b>при5 дебаркадер каюта 04</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-05aed7d8684a75e1b9.jpg</i><b>при5 дебаркадер каюта 05</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-062b0ce6b1ea8591ff.jpg</i><b>при5 дебаркадер каюта 06</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-071bb5940bd8534100.jpg</i><b>при5 дебаркадер каюта 07</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-KAYTA-085b394bf7ebbd408e.jpg</i><b>при5 дебаркадер каюта 08</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-NOS-0128e56f040acec362.jpg</i><b>при5 дебаркадер нос 01</b>');
+
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-NOS-03d9cfbd15794f683e.jpg</i><b>при5 дебаркадер нос 03</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-NOS-045eb07983004ad8ff.jpg</i><b>при5 дебаркадер нос 04</b>');
+
+
+
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-PALUBA-04e8efca29b901456c.jpg</i><b>при5 дебаркадер палуба 04</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-PALUBA-058e12444162806f17.jpg</i><b>при5 дебаркадер палуба 05</b>');
+
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-PALUBA-07462f8eee48c21148.jpg</i><b>при5 дебаркадер палуба 07</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-PALUBA-08e8cac6b0c545e74e.jpg</i><b>при5 дебаркадер палуба 08</b>');
+document.write('<i>https://imgbb.su/images/2022/10/03/PRI5-DEBARKADER-PALUBA-0927408db749e57b9e.jpg</i><b>при5 дебаркадер палуба 09</b>');
---
+++ pripyat2/detsad_radost.js
@@ -0,0 +1,5 @@
+document.write('<i>https://imgbb.su/images/2022/10/08/PRI5-DETSAD-RADOST-01f336b863f0a8e14e.jpg</i><b>при5 детсад радость 01</b>');
+document.write('<i>https://imgbb.su/images/2022/10/08/PRI5-DETSAD-RADOST-029418809387435336.jpg</i><b>при5 детсад радость 02</b>');
+document.write('<i>https://imgbb.su/images/2022/10/08/PRI5-DETSAD-RADOST-03333b69e905fba02a.jpg</i><b>при5 детсад радость 03</b>');
+document.write('<i>https://imgbb.su/images/2022/10/08/PRI5-DETSAD-RADOST-045f8dafd63594c17d.jpg</i><b>при5 детсад радость 04</b>');
+document.write('<i>https://imgbb.su/images/2022/10/08/PRI5-DETSAD-RADOST-0524ec4b2eaf2725e7.jpg</i><b>при5 детсад радость 05</b>');
